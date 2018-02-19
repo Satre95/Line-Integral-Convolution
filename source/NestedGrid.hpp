f15ac4cdf3db6c58a71a8a3b07597c53c4cd5ab2
Siddharth Atre <sidatre@gmail.com>
2018-02-18 16:41:24 -0800

    Created files for hierarchichal grid structures

---
+++ source/NestedGrid.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "UniformGrid.hpp"
+
+class NestedGrid: public UniformGrid
+{
+public:
+	NestedGrid();
+	~NestedGrid();
+	
+};
---
+++ source/UniformGrid.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+class UniformGridGeometry {
+public:
+    UniformGridGeometry() {}
+    ~UniformGridGeometry() {}
+};
+
+class UniformGrid {
+    public:
+    UniformGrid(){}
+    ~UniformGrid(){}
+};
+
+
