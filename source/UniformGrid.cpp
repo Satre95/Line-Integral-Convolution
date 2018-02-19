ecd2647b2d0e7d15183ec73701a2c68ef2ea7690
Siddharth Atre <sidatre@gmail.com>
2018-02-18 17:18:46 -0800

    Started work on Grid class

--- source/UniformGrid.cpp
+++ source/UniformGrid.cpp
@@ -1,1 +1,62 @@
 #include "UniformGrid.hpp"
+
+using namespace cinder;
+
+void UniformGridGeometry::DefineShape( size_t uNumElements , const vec3 & vMin , const vec3 & vMax , bool bPowerOf2 )
+{
+    mMinCorner  = vMin ;
+    static const float Nudge = 1.0f + FLT_EPSILON ;  // slightly expand size to ensure robust containment even with roundoff
+    mGridExtent     = ( vMax - vMin ) * Nudge ;
+    
+    vec3 vSizeEffective( GetExtent() ) ;
+    int numDims = 3 ;   // Number of dimensions to region.
+    if( 0.0f == vSizeEffective.x )
+    {   // X size is zero so reduce dimensionality
+        vSizeEffective.x = 1.0f ; // This component will not contribute to the total region volume/area/length.
+        mGridExtent.x = 0.0f ;
+        -- numDims ;
+    }
+    if( 0.0f == vSizeEffective.y )
+    {   // Y size is zero so reduce dimensionality
+        vSizeEffective.y = 1.0f ; // This component will not contribute to the total region volume/area/length.
+        mGridExtent.y = 0.0f ;
+        -- numDims ;
+    }
+    if( 0.0f == vSizeEffective.z )
+    {   // Z size is zero so reduce dimensionality
+        vSizeEffective.z = 1.0f ; // This component will not contribute to the total region volume/area/length.
+        mGridExtent.z = 0.0f ;
+        -- numDims ;
+    }
+    
+    // Compute region volume, area or length (depending on dimensionality).
+    const float volume              = vSizeEffective.x * vSizeEffective.y * vSizeEffective.z ;
+    const float cellVolumeCubeRoot  = powf( volume / float( uNumElements ) , -1.0f / float( numDims ) ) ; // Approximate size of each cell in grid.
+    // Compute number of cells in each direction of uniform grid.
+    // Choose grid dimensions to fit as well as possible, so that the total number
+    // of grid cells is nearly the total number of elements in the contents.
+    unsigned numCells[3] = { MAX2( 1 , unsigned( GetExtent().x * cellVolumeCubeRoot + 0.5f ) ) ,
+        MAX2( 1 , unsigned( GetExtent().y * cellVolumeCubeRoot + 0.5f ) ) ,
+        MAX2( 1 , unsigned( GetExtent().z * cellVolumeCubeRoot + 0.5f ) ) } ;
+    
+    if( bPowerOf2 )
+    {   // Choose number of gridcells to be powers of 2.
+        // This will simplify subdivision in a NestedGrid.
+        numCells[ 0 ] = NearestPowerOfTwo( numCells[ 0 ] ) ;
+        numCells[ 1 ] = NearestPowerOfTwo( numCells[ 1 ] ) ;
+        numCells[ 2 ] = NearestPowerOfTwo( numCells[ 2 ] ) ;
+    }
+    
+    while( numCells[ 0 ] * numCells[ 1 ] * numCells[ 2 ] >= uNumElements * 8 )
+    {   // Grid capacity is excessive.
+        // This can occur when the trial numCells is below 0.5 in which case the integer arithmetic loses the subtlety.
+        numCells[ 0 ] = MAX2( 1 , numCells[0] / 2 ) ;
+        numCells[ 1 ] = MAX2( 1 , numCells[1] / 2 ) ;
+        numCells[ 2 ] = MAX2( 1 , numCells[2] / 2 ) ;
+    }
+    mNumPoints[ 0 ] = numCells[ 0 ] + 1 ; // Increment to obtain number of points.
+    mNumPoints[ 1 ] = numCells[ 1 ] + 1 ; // Increment to obtain number of points.
+    mNumPoints[ 2 ] = numCells[ 2 ] + 1 ; // Increment to obtain number of points.
+    
+    PrecomputeSpacing() ;
+}
--- source/UniformGrid.hpp
+++ source/UniformGrid.hpp
@@ -1,9 +1,110 @@
 #pragma once
+#include <cinder/Vector.h>
 
+/*! \brief Base class for uniform grid.
+ 
+ The shape of this grid is such that the "minimal corner" point
+ resides at indices {0,0,0} and the "maximal corner" point
+ resides at indices {Nx-1,Ny-1,Nz-1}.
+ 
+ The number of /points/ in each direction i is N_i.
+ 
+ A cell is defined by the 8 points that lie at its corners.
+ 
+ This also implies that the grid must have at least 2 points in
+ each direction.
+ 
+ The number of /cells/ in each direction i is (N_i-1).
+ 
+ The size of a side i of each cell is therefore
+ s_i = (vMax-vMin)_i / (N_i-1) .
+ 
+ \verbatim
+ 
+                vMin
+                0       1     . . .      Nx-1
+                (*)------*-------*-------* 0
+                /|       |       |       |
+             . / |       |       |       |
+            . *  |       |       |       |
+           . /|  *-------*-------*-------* 1
+            / | /|       |       |       |
+      Nz-1 *  |/ |       | cell  |       |
+           |  *  |       |       |       | .
+           | /|  *-------*-------*-------* .
+           |/ | /|       |       |       | .
+           *  |/ |       |       |       |
+           |  *  |       |       |       |
+           | /|  *-------*-------*-------* Ny-1
+           |/ | /       /       /       /
+           *  |/       /       /       /
+           |  *-------*-------*-------*
+           | /       /       /       /
+           |/       /       /       /
+           *-------*-------*------(*)vMax
+ 
+ \endverbatim
+ */
 class UniformGridGeometry {
 public:
-    UniformGridGeometry() {}
+    /*!
+     \brief Construct an empty UniformGridGeometry.
+     
+     \see Clear
+     */
+    UniformGridGeometry() :
+    mMinCorner( 0.0f , 0.0f , 0.0f ), mGridExtent( 0.0f , 0.0f , 0.0f ),
+    mCellExtent( 0.0f , 0.0f , 0.0f ), mCellsPerExtent( 0.0f , 0.0f , 0.0f )
+    {
+        mNumPoints[ 0 ] = mNumPoints[ 1 ] = mNumPoints[ 2 ] = 0 ;
+    }
+    
     ~UniformGridGeometry() {}
+    
+    /*!
+     \brief Construct a uniform grid that fits the given geometry.
+     
+     \see Clear, DefineShape
+     */
+    UniformGridGeometry( unsigned uNumElements , const cinder::vec3 & vMin , const cinder::vec3 & vMax , bool bPowerOf2 = true)
+    {
+        DefineShape( uNumElements , vMin , vMax , bPowerOf2 ) ;
+    }
+    
+    /*! \brief Define the shape a uniform grid such that it fits the given geometry.
+     
+     \param uNumElements - number of elements this container will contain.
+     
+     \param vMin - minimal coordinate of axis-aligned bounding box.
+     
+     \param vMax - maximal coordinate of axis-aligned bounding box.
+     
+     \param bPowerOf2 - whether to make each grid dimension a power of 2.
+     Doing so simplifies grid subdivision, if this grid will be used in a hierarchical grid.
+     
+     This makes a uniform grid of cells, where each cell is the same size
+     and the side of each cell is nearly the same size.  If the cells are
+     3-dimensional then that means each cell is a box, nearly a cube.
+     The number of dimensions of the region depends on the actual size of
+     the region.  If any size component is smaller than a small threshold
+     then this class considers that component to be zero, and reduces the
+     dimensionality of the region.  For example, if the region size is
+     (2,3,0) then this class considers the region to have 2 dimensions
+     (x and y) since the z size is zero.
+     */
+    void DefineShape( size_t uNumElements , const cinder::vec3 & vMin , const cinder::vec3 & vMax , bool bPowerOf2);
+    
+    //Getters and Setters
+    cinder::vec3 & GetExtent() { return mGridExtent; }
+    const cinder::vec3 & GetExtent() const { return mGridExtent; }
+    
+protected:
+    cinder::vec3        mMinCorner      ;   ///< Minimum position (in world units) of grid in X, Y and Z directions.
+    cinder::vec3        mGridExtent     ;   ///< Size (in world units) of grid in X, Y and Z directions.
+    cinder::vec3        mCellExtent     ;   ///< Size (in world units) of a cell.
+    cinder::vec3        mCellsPerExtent ;   ///< Reciprocal of cell size (precomputed once to avoid excess divides).
+    unsigned            mNumPoints[ 3 ] ;   ///< Number of gridpoints along X, Y and Z directions.
+    
 };
 
 class UniformGrid {
